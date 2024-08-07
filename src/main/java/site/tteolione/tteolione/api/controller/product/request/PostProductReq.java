29ff87df02d1bdeae27985210253a5d40c0ad113
Anwooseong <aws5624@naver.com>
2024-08-07 17:28:54 +0900

    test: 상품 등록 컨트롤러 테스트

--- src/main/java/site/tteolione/tteolione/api/controller/product/request/PostProductReq.java
+++ src/main/java/site/tteolione/tteolione/api/controller/product/request/PostProductReq.java
@@ -1,6 +1,9 @@
 package site.tteolione.tteolione.api.controller.product.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import site.tteolione.tteolione.api.service.product.request.PostProductServiceReq;
@@ -14,32 +17,46 @@ public class PostProductReq {
     @NotNull(message = "카테고리 Id를 입력해주세요.")
     private Long categoryId;
 
-    @NotNull(message = "상품 제목을 입력해주세요.")
+    @NotBlank(message = "상품 제목을 입력해주세요.")
     private String title;
 
-    @NotNull(message = "구입 가격을 입력해주세요.")
+    @Positive(message = "구입 가격을 입력해주세요.")
     private int buyPrice;
 
-    @NotNull(message = "구입 수량을 입력해주세요.")
+    @Positive(message = "구입 수량을 입력해주세요.")
     private int buyCount;
 
-    @NotNull(message = "공유 가격을 입력해주세요.")
+    @Positive(message = "공유 가격을 입력해주세요.")
     private int sharePrice;
 
-    @NotNull(message = "공유 수량을 입력해주세요.")
+    @Positive(message = "공유 수량을 입력해주세요.")
     private int shareCount;
 
     @NotNull(message = "구매 일자를 입력해주세요.")
     private LocalDateTime buyDate;
 
-    @NotNull(message = "상세 설명을 입력해주세요.")
+    @NotBlank(message = "상세 설명을 입력해주세요.")
     private String content;
 
-    @NotNull(message = "경도를 입력해주세요.")
+    @Positive(message = "경도를 입력해주세요.")
     private double longitude;
 
-    @NotNull(message = "위도를 입력해주세요.")
+    @Positive(message = "위도를 입력해주세요.")
     private double latitude;
+
+    @Builder
+    public PostProductReq(@NotNull(message = "카테고리 Id를 입력해주세요.") Long categoryId, @NotNull(message = "상품 제목을 입력해주세요.") String title, @NotNull(message = "구입 가격을 입력해주세요.") int buyPrice, @NotNull(message = "구입 수량을 입력해주세요.") int buyCount, @NotNull(message = "공유 가격을 입력해주세요.") int sharePrice, @NotNull(message = "공유 수량을 입력해주세요.") int shareCount, @NotNull(message = "구매 일자를 입력해주세요.") LocalDateTime buyDate, @NotNull(message = "상세 설명을 입력해주세요.") String content, @NotNull(message = "경도를 입력해주세요.") double longitude, @NotNull(message = "위도를 입력해주세요.") double latitude) {
+        this.categoryId = categoryId;
+        this.title = title;
+        this.buyPrice = buyPrice;
+        this.buyCount = buyCount;
+        this.sharePrice = sharePrice;
+        this.shareCount = shareCount;
+        this.buyDate = buyDate;
+        this.content = content;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
 
     public PostProductServiceReq toServiceRequest() {
         return PostProductServiceReq.builder()
