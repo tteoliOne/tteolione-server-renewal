d5385b0eb7a8c32516ed258173a5faf3026258db
Anwooseong <aws5624@naver.com>
2024-08-21 17:46:20 +0900

    test: 좋아요 유무, 좋아요 추가, 좋아요 취소 서비스 로직 테스트

--- src/main/java/site/tteolione/tteolione/domain/likes/Likes.java
+++ src/main/java/site/tteolione/tteolione/domain/likes/Likes.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import site.tteolione.tteolione.domain.product.Product;
 import site.tteolione.tteolione.domain.user.User;
 
 @Entity
@@ -18,15 +19,28 @@ public class Likes {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
-    private site.tteolione.tteolione.domain.product.Product product;
+    private Product product;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Version
+    private Long version;
+
     @Builder
-    public Likes(site.tteolione.tteolione.domain.product.Product product, User user) {
+    public Likes(Product product, User user) {
         this.product = product;
         this.user = user;
+    }
+
+    public void removeLike() {
+        this.product = null;
+        this.user = null;
+    }
+
+    public void addLike(Product product) {
+        this.product = product;
+        this.user = product.getUser();
     }
 }
