abbd9d12e36ea4e0d82dbd3a88fc6133c149820b
Anwooseong <aws5624@naver.com>
2024-08-27 00:30:17 +0900

    test: 카카오 로그인 신규회원가입 테스트

--- src/test/java/site/tteolione/tteolione/api/controller/user/OAuth2ControllerTest.java
+++ src/test/java/site/tteolione/tteolione/api/controller/user/OAuth2ControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.multipart.MultipartFile;
 import site.tteolione.tteolione.ControllerTestSupport;
 import site.tteolione.tteolione.GenerateMockToken;
 import site.tteolione.tteolione.WithMockCustomAccount;
@@ -135,8 +137,138 @@ class OAuth2ControllerTest extends ControllerTestSupport {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("카카오의 accessToken을 입력해 주세요."));
     }
 
-
+    @DisplayName("카카오 로그인 신규회원 등록")
     @Test
-    void kakaoSignup() {
+    @WithMockCustomAccount
+    void kakaoSignup_Success() throws Exception {
+        //given
+        String kakaoAccessToken = "kakaoAccessToken";
+        String fcmTargetToken = "fcmTargetToken";
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+        String nickname = "nickname";
+        Long userId = 1L;
+
+        MockMultipartFile profile = new MockMultipartFile(
+                "profile",
+                "profile.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                "content".getBytes()
+        );
+
+        OAuth2KakaoReq oAuth2KakaoReq = OAuth2KakaoReq.builder()
+                .accessToken(kakaoAccessToken)
+                .targetToken(fcmTargetToken)
+                .build();
+
+        MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(oAuth2KakaoReq).getBytes()
+        );
+
+        TokenInfoRes tokenInfoRes = TokenInfoRes.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+
+        User user = User.builder()
+                .nickname(nickname)
+                .build();
+        ReflectionTestUtils.setField(user, "userId", userId);
+        LoginRes response = LoginRes.fromKakao(tokenInfoRes, user);
+
+        BDDMockito.when(oAuth2Service.signUpKakao(profile, oAuth2KakaoReq.toServiceRequest())).thenReturn(response);
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v2/auth/kakao/profile")
+                        .file(profile)
+                        .file(request)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(Code.OK.getCode()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Ok"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.existsUser").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId").value(userId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.accessToken").value(accessToken))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.refreshToken").value(refreshToken))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.nickname").value(user.getNickname()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.appleRefreshToken").doesNotExist());
+    }
+
+    @DisplayName("카카오 로그인 신규회원 등록 - 카카오 액세스 토큰 유효성 검사 실패")
+    @Test
+    @WithMockCustomAccount
+    void kakaoSignup_Failure_Validate_KakaoAccessToken() throws Exception {
+        //given
+        String accessToken = "accessToken";
+        OAuth2KakaoReq oAuth2KakaoReq = OAuth2KakaoReq.builder()
+                .accessToken(accessToken)
+                .targetToken(null)
+                .build();
+
+        MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(oAuth2KakaoReq).getBytes()
+        );
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v2/auth/kakao/profile")
+                        .file(request)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(Code.BAD_REQUEST.getCode()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("필수 요청 파트 'profile'가 누락되었습니다."));
+    }
+
+    @DisplayName("카카오 로그인 신규회원 등록 - 카카오 액세스 토큰 유효성 검사 실패")
+    @Test
+    @WithMockCustomAccount
+    void kakaoSignup_Miss_Profile() throws Exception {
+        //given
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+        String nickname = "nickname";
+        Long userId = 1L;
+
+        MockMultipartFile profile = new MockMultipartFile(
+                "profile",
+                "profile.jpg",
+                MediaType.IMAGE_JPEG_VALUE,
+                "content".getBytes()
+        );
+
+        OAuth2KakaoReq oAuth2KakaoReq = OAuth2KakaoReq.builder()
+                .accessToken(null)
+                .targetToken(null)
+                .build();
+
+        MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(oAuth2KakaoReq).getBytes()
+        );
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v2/auth/kakao/profile")
+                        .file(profile)
+                        .file(request)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(Code.VALIDATION_ERROR.getCode()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("카카오의 accessToken을 입력해 주세요."));
     }
 }
