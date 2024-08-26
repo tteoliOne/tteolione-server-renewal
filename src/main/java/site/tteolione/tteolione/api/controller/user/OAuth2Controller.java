e5f0a9e5fccc3c29358011849958f6feec7c1f18
Anwooseong <aws5624@naver.com>
2024-08-26 21:54:12 +0900

    fix: 카카오 액세트 토큰 @Valid 추가

--- src/main/java/site/tteolione/tteolione/api/controller/user/OAuth2Controller.java
+++ src/main/java/site/tteolione/tteolione/api/controller/user/OAuth2Controller.java
@@ -1,5 +1,6 @@
 package site.tteolione.tteolione.api.controller.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +20,16 @@ public class OAuth2Controller {
     private final OAuth2Service oAuth2Service;
 
     @PostMapping("/kakao")
-    public BaseResponse<LoginRes> kakaoLogin(@RequestBody OAuth2KakaoReq request) {
+    public BaseResponse<LoginRes> kakaoLogin(@Valid @RequestBody OAuth2KakaoReq request) {
         LoginRes response = oAuth2Service.validateKakaoAccessToken(request.toServiceRequest());
         return BaseResponse.of(response);
     }
 
     @PostMapping(path = "/kakao/profile", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public BaseResponse<LoginRes> kakaoSignup(@RequestPart(value = "profile") MultipartFile profile,
-                                                   @RequestPart(value = "request") OAuth2KakaoReq request) {
+    public BaseResponse<LoginRes> kakaoSignup(
+            @RequestPart(value = "profile") MultipartFile profile,
+            @Valid @RequestPart(value = "request") OAuth2KakaoReq request
+    ) {
         LoginRes response = oAuth2Service.signUpKakao(profile, request.toServiceRequest());
         return BaseResponse.of(response);
     }
