364f7b07f4a88104064ade84f8315f8dcd14aef5
Anwooseong <aws5624@naver.com>
2024-08-27 16:29:58 +0900

    feat: 카카오 로그인 신규회원 기존회원 기능 완료

--- src/main/java/site/tteolione/tteolione/api/controller/user/OAuth2Controller.java
+++ src/main/java/site/tteolione/tteolione/api/controller/user/OAuth2Controller.java
@@ -19,12 +19,18 @@ public class OAuth2Controller {
 
     private final OAuth2Service oAuth2Service;
 
+    /**
+     * 카카오 로그인(신규회원일 때는 act, rtk null 반환, 기존회원일 때는 act, rtk 반환)
+     */
     @PostMapping("/kakao")
     public BaseResponse<LoginRes> kakaoLogin(@Valid @RequestBody OAuth2KakaoReq request) {
         LoginRes response = oAuth2Service.validateKakaoAccessToken(request.toServiceRequest());
         return BaseResponse.of(response);
     }
 
+    /**
+     * 카카오 로그인 신규 회원가입 로그인처리
+     */
     @PostMapping(path = "/kakao/profile", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public BaseResponse<LoginRes> kakaoSignup(
             @RequestPart(value = "profile") MultipartFile profile,
