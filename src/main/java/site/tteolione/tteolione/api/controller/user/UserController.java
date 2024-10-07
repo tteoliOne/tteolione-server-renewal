d66dedfa06d51a43dbe51f4927e4afd735a93996
Anwooseong <aws5624@naver.com>
2024-10-07 20:50:14 +0900

    feat: 비밀번호 찾기 기능 개발 완료

--- src/main/java/site/tteolione/tteolione/api/controller/user/UserController.java
+++ src/main/java/site/tteolione/tteolione/api/controller/user/UserController.java
@@ -70,6 +70,9 @@ public class UserController {
         return BaseResponse.of(userService.verifyLoginId(request.toServiceRequest()));
     }
 
+    /**
+     * 비밀번호 찾기
+     */
     @PostMapping("/find/password")
     public BaseResponse<String> findPassword(@Valid @RequestBody FindPasswordReq request) throws MessagingException {
         return BaseResponse.of(userService.findPassword(request.toServiceRequest()));
